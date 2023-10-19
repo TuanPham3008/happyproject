a37f52a3d1b9006b8811d55769deb3be1bba6d4e
Your Name <you@example.com>
2023-10-20 00:01:48 +0700

    first commit

---
+++ assets/js/config.js
@@ -0,0 +1,12 @@
+const nameGirl = '💘Mèo Con💘';
+const giftUrl = '';
+const eventName = 'Mừng Ngày 20-10';
+const titleCard = 'Tặng em Mèo Con';
+const contentCard = 'Anh tin rằng em luôn là một cô gái mạnh mẽ và có thể đương đầu với mọi thử thách trong cuộc sống. Chúc em gái của anh có một ngày 20/10 tràn ngập niềm vui và quên hết những chuyện buồn trong quá khứ. Mong rằng những điều tốt đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau!';
+
+const contentLetterSrart_actived = "Nhân ngày 20/10 anh có món quà này dành tặng em mèo con. Nhấn vào icon dưới đây nhé!"
+
+
+const giftImage = './assets/img/Meo_Con1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
---
+++ assets/js/startform.js
@@ -0,0 +1,37 @@
+
+// Gắn 1 đường link ảnh bất kì
+let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
+imgStart.src = "./assets/img/cute-young-boy-kid-wearing-vest-and-hat-free-png.png";
+
+// Gắn 1 link ảnh bất kì
+let imgLetter = document.querySelector(".img");
+
+const splitContentLetterSrart_actived = contentLetterSrart_actived.split("");
+
+//Hiệu ứng gõ chữ cho phần mở đầu của bức thư
+document.querySelector(".sticker").addEventListener("click", function () { 
+    document.querySelector(".contentLetter").innerHTML = "";
+    document.querySelector(".startLetter").classList.add("active")
+    setTimeout(() => {
+        splitContentLetterSrart_actived.forEach((val, index) => {
+            setTimeout(() => {
+                document.querySelector(".contentLetter").innerHTML += val;
+                if (index == contentLetterSrart_actived.length - 1) {
+                    setTimeout(() => {
+                        document.querySelector(".recieve").setAttribute("style", "opacity: 1; transition: .5s") 
+                    }, 1000)
+                }
+            }, 50 * index)
+        })
+    }, 1000)
+})
+
+document.querySelector(".recieve").addEventListener("click", () => {
+    document.querySelector(".startLetter").classList.add("close");
+    setTimeout(() => {
+        document.querySelector(".startForm").classList.add("close");
+        setTimeout(() => {
+            document.querySelector(".startForm").classList.add('startactive');
+        }, 300)
+    }, 300)
+})
